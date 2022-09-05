fcce32d9777cbbf73e927a164dcae054c242dbcc
vagrant <tomoyuki.koube@grrow.jp>
2022-09-05 21:38:22 +0900

    Change netlify-url for netlify cms, at the admin/config.yml

--- tools/request.sh
+++ tools/request.sh
@@ -6,9 +6,9 @@ KeyPair='CloudFront-Key-Pair-Id'
 
 expires='2080403806'
 signature=`cat policy.json | tr -d "\n" | tr -d " \t\n\r" | openssl sha1 -sign .secret/private.pem | openssl base64 -A | tr -- '+=/' '-_~' | tr -d '\n'`
-keypairid='KBE5TAWE5BCYY'
+keypairid='KP1F0KGTEC9XX'
 
-target='https://d1f6eibigcfyw1.cloudfront.net'
+target='https://dt2qnziicrcht.cloudfront.net'
 
 CMD="curl -H \"Cookie:${Expires}=${expires};${Signature}=${signature};${KeyPair}=${keypairid};\" ${target}"
 echo $CMD
